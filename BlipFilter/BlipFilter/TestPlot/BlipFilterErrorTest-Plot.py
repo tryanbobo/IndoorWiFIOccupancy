dd68e366d0860eacd456f67ba859fa474cb4c7ef
tb1302 <tb1302@txstate.edu>
2023-04-18 10:36:46 -0500

    Filter improvements that no longer deletes a user with timedelta > 1hr. Created  building visit index to sort by visit.

--- BlipFilter/BlipFilter/TestPlot/BlipFilterErrorTest-Plot.py
+++ BlipFilter/BlipFilter/TestPlot/BlipFilterErrorTest-Plot.py
@@ -56,13 +56,13 @@ def plot_time_series(df, title):
         rmse = math.sqrt(mean_squared_error(cleanDaily_df['GroundTruthFloor'], cleanDaily_df['Corrected_Floor']))
 
         # Plot the time series for the specific date
-        axs[i].plot(daily_df.index, daily_df['GroundTruthFloor'], '-', label='GroundTruthFloor')
-        axs[i].plot(daily_df.index, daily_df['Floor'], '--', label='Floor')
-        axs[i].plot(daily_df.index, daily_df['Corrected_Floor'], ':', label='Corrected_Floor')
+        axs[i].plot(daily_df.index, daily_df['GroundTruthFloor'], '-', label='Floor: Ground Truth')
+        axs[i].plot(daily_df.index, daily_df['Floor'], '--', label='Floor: Wi-Fi')
+        axs[i].plot(daily_df.index, daily_df['Corrected_Floor'], ':', label='Floor: Wi-Fi Corrected')
 
         axs[i].set_xlabel('Datetime')
         axs[i].set_ylabel('Floor')
-        axs[i].set_title(f'{date.strftime("%Y-%m-%d")} (RMSE: {rmse:.2f})', fontsize=10)
+        axs[i].set_title(f'{date.strftime("%Y-%m-%d")} (RMSE: {rmse:.2f})', fontsize=10, fontweight='bold')
 
         # Format x-axis ticks to display hour and minute
         axs[i].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
@@ -82,21 +82,25 @@ def plot_time_series(df, title):
     fig.subplots_adjust(hspace=0.326, bottom=0.067, top=0.905)
 
     # Set main title for entire plot with overall RMSE using plt.annotate
-    plt.annotate(f'{title} (Overall RMSE: {overall_rmse:.2f})', xy=(0.5, 0.98), xycoords='figure fraction', ha='center',
-                 va='top', fontsize=16)
+    #plt.annotate(f'{title} (Overall RMSE: {overall_rmse:.2f})', xy=(0.5, 0.99), xycoords='figure fraction', ha='center',
+    #             va='top', fontsize=16)
+    # Set main title for entire plot with overall RMSE using fig.suptitle
+    fig.suptitle(f'{title} (Overall RMSE: {overall_rmse:.2f})', fontsize=16, fontweight='bold', y=0.96)
 
     # Save plot as png
     outputFile = r'C:\Users\tb1302\OneDrive - Texas State University\IndStudy_Bobo\spring2023\BlipFilter\output\figures'
 
-    outputFile = os.path.join(outputFile,f'{title}.png')
+    outputFile = os.path.join(outputFile,f"{title.lstrip('Blip Filter:')}.png"
+    )
     plt.savefig(outputFile, bbox_inches='tight')
     print(f"Figure {title} saved at {outputFile}")
-    plt.close(fig)
     #plt.show()
+    plt.close(fig)
+
 
 
 # Define function to correct Floor column with variable thresholds
-def correct_floor_v2(df, threshold_time_on_current_floor, threshold_last_floor_time):
+def correct_floor_time_threshold(df, threshold_time_on_initial_floor, threshold_time_on_succeeding_floor):
     # Initialize variables
     floor = df.iloc[0]['Floor']
     last_floor = floor
@@ -108,8 +112,8 @@ def correct_floor_v2(df, threshold_time_on_current_floor, threshold_last_floor_t
     for i, row in df.iterrows():
         # Condition 1: Check if floor has changed
         if row['Floor'] != floor:
-            # Check if previous floor was correct (change of less than threshold_last_floor_time minutes and occupancy of at least threshold_time_on_current_floor minutes on the floor)
-            if (row['Datetime'] - last_floor_time).seconds / 60 < threshold_last_floor_time and time_on_current_floor >= threshold_time_on_current_floor:
+            # Check if previous floor was correct (change of less than threshold_time_on_succeeding_floor minutes and occupancy of at least threshold_time_on_initial_floor minutes on the floor)
+            if (row['Datetime'] - last_floor_time).seconds / 60 < threshold_time_on_succeeding_floor and time_on_current_floor >= threshold_time_on_initial_floor:
                 # Set corrected floor to previous floor
                 corrected_floor.append(last_floor)
                 floor = last_floor
@@ -147,9 +151,9 @@ def correct_isolated_floor_changes(df):
 # Main loop iterating through threshold combinations
 results = []
 
-for threshold_time_on_current_floor in range(0, 6):
-    for threshold_last_floor_time in range(0, 6):
-        corrected_floor_dfs = [correct_floor_v2(group, threshold_time_on_current_floor, threshold_last_floor_time) for _, group in grouped]
+for threshold_time_on_initial_floor in range(0, 4):
+    for threshold_time_on_succeeding_floor in range(0, 4):
+        corrected_floor_dfs = [correct_floor_time_threshold(group, threshold_time_on_initial_floor, threshold_time_on_succeeding_floor) for _, group in grouped]
         df_combined = pd.concat(corrected_floor_dfs)
         df_combined = correct_isolated_floor_changes(df_combined)
 
@@ -163,15 +167,15 @@ for threshold_time_on_current_floor in range(0, 6):
 
         # Store results
         results.append({
-            'Initial floor threshold': threshold_last_floor_time,
-            'Succeeding floor threshold ': threshold_time_on_current_floor,
+            'Initial floor threshold': threshold_time_on_succeeding_floor,
+            'Succeeding floor threshold ': threshold_time_on_initial_floor,
             'mse': mse,
             'mae': mae,
             'rmse': rmse
         })
         # Plot the time series for the current combination (only for the first user for demonstration purposes)
-        if threshold_time_on_current_floor < 6 and threshold_last_floor_time < 6:
-            plot_title = f' Blip Filter; Initial floor threshold = {threshold_last_floor_time}-minutes and Succeeding floor threshold = {threshold_time_on_current_floor}-minutes'
+        if threshold_time_on_initial_floor < 6 and threshold_time_on_succeeding_floor < 6:
+            plot_title = f' Blip Filter: Initial floor threshold = {threshold_time_on_succeeding_floor}-minutes and Succeeding floor threshold = {threshold_time_on_initial_floor}-minutes'
             plot_time_series(df_combined[df_combined['user'] == df_combined['user'].unique()[0]], plot_title)
 
 # Convert results to a DataFrame
@@ -199,4 +203,6 @@ mae = mean_absolute_error(df_clean['GroundTruthFloor'], df_clean['Floor'])
 
 print('MSE of raw Wi-Fi and Ground Truth = {}'.format(mse))
 print('MAE of raw Wi-Fi and Ground Truth = {}'.format(mae))
+
+#print(results_df)
 
--- Non-Human-Filter/Mobility_Non-human2.py
+++ Non-Human-Filter/Mobility_Non-human2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # show full columns at print
 pd.set_option('display.max_colwidth', None)
@@ -7,6 +8,8 @@ pd.set_option('display.max_colwidth', None)
 path = r'C:\Users\tb1302\OneDrive - Texas State University\IndStudy_Bobo\Data\WifiData\output'
 df = pd.read_csv(path + "\Preprocessed.alk_data_23_02_19_to_23_04_03.csv")
 
+# Create sub-set to test data
+df = df.head(100000)
 # convert Datetime column to datetime type
 df['Datetime'] = pd.to_datetime(df['Datetime'])
 
@@ -17,26 +20,51 @@ df = df.sort_values('Datetime')
 df['time_diff'] = df.groupby(['user', 'Floor'])['Datetime'].diff()
 print(df)
 
-# filter out records where time difference is greater than 1 hour
-df = df[df['time_diff'] < pd.Timedelta(hours=1)]
 
+def generate_visit_id(time_diff_series):
+    visit_id = pd.Series(np.zeros(len(time_diff_series), dtype=int), index=time_diff_series.index)
+    current_visit_id = 1
+
+    for i in range(1, len(time_diff_series)):
+        if pd.isna(time_diff_series.iloc[i]) or time_diff_series.iloc[i] >= pd.Timedelta(hours=4):
+            current_visit_id += 1
+        visit_id[i] = current_visit_id
+
+    return visit_id
+
+
+# Apply the custom function to create the 'visit_id' column
+df['visit_id'] = 0
+for _, group in df.groupby(['user', 'Floor']):
+    group_index = group.index
+    visit_id = generate_visit_id(group['time_diff'])
+    df.loc[group_index, 'visit_id'] = visit_id
+print(df)
 # calculate the stay time for each user on each floor per visit
-df['stay_time'] = df.groupby(['user', 'Floor'])['time_diff'].cumsum()
-
-# calculate the total stay time for each user on each floor
-df['total_stay_time'] = df.groupby(['user', pd.Grouper(key='Datetime', freq='24H')])['stay_time'].transform('sum')
+df['stay_time'] = df.groupby(['user', 'Floor', 'visit_id'])['time_diff'].cumsum()
 
 #calculate the daily stay time for each user
 df['daily_stay_time'] = df.groupby(['user', pd.Grouper(key='Datetime', freq='24H')])['stay_time'].transform('sum')
 
+
+####################Check distribution before filters########################
+# Plot histogram of time differences
+time_diff_minutes = df['time_diff'].dt.total_seconds() / 60
+time_diff_minutes.plot.hist(bins=100, log=True)
+plt.xlabel('Time difference (minutes)')
+plt.title('Histogram of time differences between consecutive records')
+plt.show()
+
+
 # Non-Human Filter: filter out records where total stay time is greater than 12 hours
 df = df[df['daily_stay_time'] < pd.Timedelta(hours=12)]
+print('After Non-Human Filter: ',len(df))
 
 # Mobility Filter: filter out records where stay time is less than 5 minutes
 df = df[df['stay_time'] >= pd.Timedelta(minutes=5)]
-
+print('After Mobility Filter: ',len(df))
 # group by user and floor, and keep only the first connection time for each visit
-filtered_data = df.groupby(['user', 'Floor'], as_index=False).agg({
+filtered_data = df.groupby(['user', 'Floor', 'visit_id'], as_index=False).agg({
     'stay_time': 'max',
     'Datetime': 'min',
     'vlan_role': 'first'
@@ -48,21 +76,12 @@ filtered_data = filtered_data.sort_values(['user', 'Datetime'])
 # export to csv
 filtered_data.to_csv(path + r'\Mobility_Non-humanFilter.csv')
 
-
-## note: freq='H' for and hour; freq='30T' for 30-min
-#floor_occupancy = filtered_data[filtered_data['Floor'] == 4].groupby([pd.Grouper(key='Datetime', freq='H')])[
-#    'user'].count()
-#print(floor_occupancy)
-
-
-################################Plotting##############################################
-
+print(filtered_data.head())
 # Find the unique floors in the filtered_data and sort them
 unique_floors = sorted(filtered_data['Floor'].unique())
 
 # Create a line plot for each floor's occupancy and an additional plot for total daily visits
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10, 12), sharex=True)
-
 
 # Plot the occupancy for each floor
 for floor in unique_floors:
@@ -76,37 +95,25 @@ ax1.set_title("Floor Occupancy (1-hour intervals)")
 ax1.legend(loc='upper right')
 
 # Calculate and plot the total daily visits for all floors
-total_daily_visits = filtered_data.groupby(pd.Grouper(key='Datetime', freq='D'))['user'].count()
-total_daily_visits.plot(ax=ax2, marker='o')
+total_daily_visits = filtered_data.groupby([pd.Grouper(key='Datetime', freq='D')])['user'].count()
+if not total_daily_visits.empty:
+    total_daily_visits.plot(ax=ax2, linestyle='-', marker='o', linewidth=2)
+else:
+    print("No data to plot for total daily visits.")
 
 # Customize the second plot
-ax2.set_ylabel("Total Visits")
-ax2.set_xlabel("Datetime")
+ax2.set_ylabel("Number of Users")
 ax2.set_title("Total Daily Visits")
 
-# Show the plots
-plt.tight_layout()
-plt.show()
-"""
-#Plots each floors occupancy counts per hour
-# plot the data for each floor
-# Find the unique floors in the filtered_data
-unique_floors = sorted(filtered_data['Floor'].unique())
+# Set the x-axis label for both plots
+fig.text(0.5, 0.04, 'Date', ha='center', va='center')
 
-# Create a line plot for each floor's occupancy
-fig, ax = plt.subplots(figsize=(10, 6))
 
-for floor in unique_floors:
-    floor_occupancy = filtered_data[filtered_data['Floor'] == floor].groupby(
-        [pd.Grouper(key='Datetime', freq='30T')])['user'].count()
-    floor_occupancy.plot(ax=ax, label=f'Floor {floor}')
+# Display the plots
+# Check if the filtered_data DataFrame is empty
+if not filtered_data.empty:
+    # Display the plots
+    plt.show()
+else:
+    print("No data to plot after applying the filters.")
 
-# Customize the plot
-ax.set_ylabel("Number of Users")
-ax.set_xlabel("Datetime")
-ax.set_title("Floor Occupancy (30-minute intervals)")
-ax.legend(loc='upper right')
-
-# Show the plot
-plt.show()
-"""
